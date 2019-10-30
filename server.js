33e716cf14cbeb8cb8536c59b3fe59df103bc4a2
Kevin Halstead <mrmichael423@icloud.com>
2019-10-30 15:39:46 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var app = express();
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/kdvrscraper", { useNewUrlParser: true });
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
